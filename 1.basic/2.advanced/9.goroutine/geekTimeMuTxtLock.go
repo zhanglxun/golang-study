2116d75786875b333ae548685b037a756cf7d4d8
jarvis <zhanglxun@qq.com>
2020-10-15 09:21:27 +0800

    muTex

--- 1.basic/2.advanced/9.goroutine/geekTimeMuTxtLock.go
+++ 1.basic/2.advanced/9.goroutine/geekTimeMuTxtLock.go
@@ -12,6 +12,11 @@ import (
 
 */
 func main() {
+
+	useLock()
+}
+
+func noLock() {
 	var count = 0
 	// 使用WaitGroup等待10个goroutine完成
 	var wg sync.WaitGroup
@@ -28,5 +33,29 @@ func main() {
 	// 等待10个goroutine完成
 	wg.Wait()
 	fmt.Println(count)
+}
 
+func useLock() {
+	// 互斥锁保护计数器
+	var mu sync.Mutex
+	// 计数器的值
+	var count = 0
+
+	// 辅助变量，用来确认所有的goroutine都完成
+	var wg sync.WaitGroup
+	wg.Add(10)
+	// 启动10个gourontine
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			// 累加10万次
+			for j := 0; j < 100000; j++ {
+				mu.Lock()
+				count++
+				mu.Unlock()
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Println(count)
 }
