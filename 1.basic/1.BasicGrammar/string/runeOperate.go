26cfc9138454151b2fe81257268a4429d1e26811
jarvis <zhanglxun@qq.com>
2021-08-07 15:36:00 +0800

    目录项目的结构学习，系统优雅停机的一个案例

---
+++ 1.basic/1.BasicGrammar/string/runeOperate.go
@@ -0,0 +1,12 @@
+package main
+
+/**
+rune 类型，就是字符串，char
+*/
+
+func main() {
+	var c rune = 32
+	var a = 32
+	println(c)
+	println(a)
+}
--- 1.basic/1.BasicGrammar/string/stringOperate.go
+++ 1.basic/1.BasicGrammar/string/stringOperate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"golangStudy/1.basic/1.BasicGrammar/access"
+	"unicode/utf8"
 )
 
 //注意：只有首字母大写的，是共有的，可以在外部访问
@@ -15,7 +16,15 @@ func accessCall() {
 
 }
 
+func stringLen() {
+	var name = "你好"
+	println(len(name))
+	//计算字符个数，要用这个。
+	println(utf8.RuneCountInString(name))
+}
+
 func main() {
 
 	accessCall()
+	stringLen()
 }
