6e96349c626808e08c159f578048ea9c65a2e5fe
jarvis <zhanglxun@qq.com>
2022-06-02 22:04:01 +0800

    更新泛型的学习使用

--- 1.basic/2.advanced/18.genericsExample/googleDemo.go
+++ 1.basic/2.advanced/18.genericsExample/googleDemo.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func SumInt32(x, y int32) int32 {
 	return x + y
@@ -9,9 +11,35 @@ func Float64(x, y float64) float64 {
 	return x + y
 }
 
+//使用泛型进行输出
 func Add[T int64 | float64](x, y T) T {
 	return x + y
 }
+
+//使用泛型进行输出
+func printSlice[T any](s []T) {
+	for _, v := range s {
+		fmt.Printf("%v ", v)
+	}
+	fmt.Println("")
+}
+
+type Addable interface {
+	int | int8 | int32 | float32 | float64 |
+		complex64 | complex128 | string
+}
+
+//type Addable2 interface {type int,interface{} }
+
+func addGenerics[T Addable](a, b T) T {
+	return a + b
+}
+
+//type Addable interface{
+//	type int, int8, int16, int32, int64,
+//	uint, uint8, uint16, uint32, uint64, uintptr,
+//	float32, float64, complex64, complex128,    string
+//}
 
 func main() {
 
@@ -19,4 +47,12 @@ func main() {
 	fmt.Println(Float64(30.79, 43.77))
 	fmt.Println(Add(31.79, 41.77))
 
+	printSlice[int]([]int{1, 2, 3, 4, 5})
+	printSlice[float64]([]float64{1.01, 2.02, 3.03, 4.04, 5.05})
+	printSlice([]string{"Hello", "World"})
+	printSlice[int64]([]int64{5, 4, 3, 2, 1})
+
+	fmt.Println(addGenerics(3.33, 6.98))
+	fmt.Println(addGenerics(30, 40))
+	fmt.Println(addGenerics("jarvis", "hello"))
 }
